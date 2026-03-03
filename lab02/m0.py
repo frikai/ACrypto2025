442f95a386c7482c79dc6c379b4fd675b61907d7
Julian Modanese <julian.modanese@gmail.com>
2026-03-03 10:04:18 +0100

    start lab2

---
+++ lab02/m0.py
@@ -0,0 +1,11 @@
+def xor(X: bytes | bytearray, Y: bytes | bytearray) -> bytes:
+    return bytes(x ^ y for (x, y) in zip(X, Y))
+
+
+def pad(X: bytes | bytearray, k: int) -> bytes:
+    klmk = k - (len(X) % k)
+    p = [klmk] * klmk
+    return bytes([x for x in X] + p)
+
+
+print(pad(b"flag", 16).hex())
